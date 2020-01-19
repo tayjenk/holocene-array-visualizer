c2f5997040d6364ca186563d1696a381cd16de1b
Tayla Monet <taymjenk@gmail.com>
2020-01-19 17:18:43 -0500

    base graph

--- src/ArrayGraph.js
+++ src/ArrayGraph.js
@@ -1,20 +1,42 @@
-import React from 'react'
-import Chart from 'chart.js'
+import React from "react"
+import { Line } from "react-chartjs-2"
 
-export default class ArrayGraph extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const inputData = [
+  { a: 1, b: 3, c: 10 },
+  { a: 3, b: 20, c: 12 },
+  { a: -1, b: -5, c: -4 }
+]
 
+const xLabels = []
+inputData.map(dataObj => xLabels.push(dataObj.a))
+console.log(xLabels)
+
+const bPoints = []
+inputData.map(dataObj => bPoints.push(dataObj.b))
+console.log(bPoints)
+const cPoints = []
+inputData.map(dataObj => cPoints.push(dataObj.c))
+console.log(cPoints)
+const state = {
+  labels: xLabels.sort((a,b) => a - b),
+  datasets: [
+    {
+      label: "B Points",
+      data: bPoints
+    },
+    {
+      label: "C Points",
+      data: cPoints
+    }
+  ]
 }
 
-const LineChart = require("react-chartjs").Line(props)
-
-const ArrayGraph = React.createClass({
-  render: function() {
+export default class ArrayGraph extends React.Component {
+  render() {
     return (
-      <LineChart
+      <div>
+        <Line data={state} />
+      </div>
     )
   }
-})
-
+}
